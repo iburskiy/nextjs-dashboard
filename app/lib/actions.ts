eaa724add411a37e6aa7012f9a04eee128dba8fd
iburskiy <iburskiy@gmail.com>
2024-01-09 11:37:45 +0300

    test commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -42,7 +42,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     status: formData.get('status'),
   });
 
-  console.log('validatedFields: ', validatedFields);
+  console.log('validatedFields:', validatedFields);
 
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
